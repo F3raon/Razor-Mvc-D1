482f3f64217fcd35ccc6d499a2df82580df5dd66
F3raon <ah4482336@gmail.com>
2025-07-25 00:58:28 +0300

    رسالة توضح التعديل

---
+++ Razor Day1/Pages/Form Registration.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Razor_Day1.Pages
+{
+    public class Form_RegistrationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
